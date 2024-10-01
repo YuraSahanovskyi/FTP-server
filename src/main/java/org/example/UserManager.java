79689b7462454b01d819702df619f9aa05051feb
Yurii Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-10-01 16:30:16 +0300

    Lab 2

---
+++ src/main/java/org/example/UserManager.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class UserManager {
+    public UserManager() {
+
+    }
+    public User authenticate(String username, String password){
+        return null;
+    }
+}
