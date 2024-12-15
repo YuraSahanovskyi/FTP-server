45ef622c3bafd49ba42391a37a46b8e5f2cb472d
Yura Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-12-15 13:00:22 +0000

    Database connection in containers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import org.example.authentication.database.DatabaseConnection;
 import org.example.connection.ConnectionHandler;
 
 import java.io.*;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,7 +17,13 @@ public class Main {
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
         ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
-
+        try {
+//            Class.forName("org.postgresql.Driver");
+            Connection connection = DatabaseConnection.getConnection();
+            System.out.println("Connected to database" + connection.toString());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("FTP Server started on port " + PORT);
 
---
+++ src/main/java/org/example/authentication/database/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package org.example.authentication.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:postgresql://" + System.getenv("DATABASE_HOST") + ":" + System.getenv("DATABASE_PORT") + "/" + System.getenv("DATABASE_NAME");
+    private static final String USER = System.getenv("DATABASE_USER");
+    private static final String PASSWORD = System.getenv("DATABASE_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
